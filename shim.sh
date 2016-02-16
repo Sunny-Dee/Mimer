be3f129aa3a4c1096f4ce00941691ca31e74b3f4
Deliana Escobari <delianaescobari@gmail.com>
2016-02-16 14:22:06 -0600

    Create and init repo

---
+++ shim.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# This is shim.bat
+echo "We are now in a shim called from the Web Browser"
+echo Arg one is: "$1"
+# Change this to point to your MyWebServer directory:
+cd /users/delianescobari/Documents/Mimer/
+# pass the name of the first argument to java:
+java -classpath ./xstream-1.2.1.jar:./xpp3_min-1.1.3.4.jar:. -Dfirstarg=$1
+#BCHandler
+read -p "Called Java: press [Enter] key to continue..."
